9c694cc7bb6cd397dc05c77966551a66664a70cd
DianaDev1310 <fujishiro.yohei@gmail.com>
2022-05-10 21:33:29 +0900

    feat: print prompt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::io;
+use std::io::Write;
 
 fn main() {
-    println!("Enter the command.");
+    print_prompt();
 
     let ans = get_input();
 
@@ -12,4 +13,9 @@ fn get_input() -> String {
     let mut command = String::new();
     io::stdin().read_line(&mut command).ok();
     return command.trim().to_string();
+}
+
+fn print_prompt() {
+    print!("jit$ ");
+    std::io::stdout().flush().unwrap();
 }
