2a2940c704898be0a1ebdc6fa2ce5dd061c8c84f
DianaDev1310 <fujishiro.yohei@gmail.com>
2022-05-12 23:37:31 +0900

    feat: echo command(mock)

--- src/prompt.rs
+++ src/prompt.rs
@@ -10,6 +10,8 @@ pub fn start_jit_prompt() {
 
         let vec: Vec<&str> = cmd.split_whitespace().collect();
         println!("{:?}", vec);
+
+        exec_command(vec);
     }
 }
 
@@ -22,4 +24,25 @@ fn get_input() -> String {
 fn print_prompt() {
     print!("jit$ ");
     std::io::stdout().flush().unwrap();
+}
+
+fn exec_command(cmd: Vec<&str>) {
+    let cmd_type = parse_command();
+
+    if cmd_type == "echo" {
+        for i in 1..=cmd.len() - 1 {
+            print!("{}", cmd[i]);
+            if i < cmd.len() - 1 {
+                print!(" ");
+                std::io::stdout().flush().unwrap();
+            }
+        }
+        println!();
+    }
+}
+
+fn parse_command() -> String {
+    let mut cmd_type = String::new();
+    cmd_type = String::from("echo");
+    return cmd_type;
 }
